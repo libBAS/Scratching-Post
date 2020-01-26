0058ac62f4da3dbd7a28eefee0139d3b25eafe80
Amalia <schw1818@umn.edu>
2020-01-25 19:13:40 -0600

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+def detect_labels(path):
+    """Detects labels in the file."""
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+
+    for label in labels:
+        print(label.description)
+
+detect_labels("pets.jpg")
+print("next")
+detect_labels("dog.jpg")
+
