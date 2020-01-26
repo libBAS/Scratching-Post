f58949bb691ffd9e1d0867ecf271ae8e93f75b32
Amalia <schw1818@umn.edu>
2020-01-25 21:05:36 -0600

    added cat and dog puns

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import os, random, csv, random
+
 def detect_labels(path):
     """Detects labels in the file."""
     from google.cloud import vision
@@ -25,10 +27,37 @@ def is_cat(labels):
             return True
     return False
 
+def get_dog_pun(data):
+    num_dog_puns = 108
+    num = random.randint(0,num_dog_puns-1)
+    pun = data[num][0]
+    nc = 0
+    for char in pun:
+        if char == ".":
+            break
+        nc+=1
+    pun = pun[nc+2:]
+    return pun
+    
+def get_cat_pun(data):
+    num_cat_puns = 66 # q odd a even up to 65,66
+    num = random.randint(0,num_cat_puns-1)
+    if num%2 != 0:
+        num -= 1
+        if num < 0:
+            num = 0
+    pun = data[num][1]
+    nc = 0
+    for char in pun:
+        if char == ".":
+            break
+        nc+=1
+    pun = pun[nc+2:]
+    return [pun, data[num+1][1]]
 
-
-
-labels = detect_labels("cat.jpg")
+path = random.choice(os.listdir("./images/images"))
+labels = detect_labels("./images/images/" + path)
+# labels = detect_labels("cat.jpg")
 for label in labels:
     print(label.description)
 print(is_dog(labels))
@@ -36,3 +65,19 @@ print(is_cat(labels))
 # print("next")
 # detect_labels("dog.jpg")
 
+pun_path = "minnehack_scraped_data.csv"
+csvDataFile = open(pun_path)
+data=list(csv.reader(csvDataFile))
+
+
+print(data[0])
+print(get_dog_pun(data))
+print(get_dog_pun(data))
+print(get_dog_pun(data))
+print(get_dog_pun(data))
+print(get_dog_pun(data))
+
+print(get_cat_pun(data))
+print(get_cat_pun(data))
+print(get_cat_pun(data))
+print(get_cat_pun(data))
